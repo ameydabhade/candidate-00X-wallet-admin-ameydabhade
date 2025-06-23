687a26715ae5c46b114d69332d924b5269fc80b0
Amey Dabhade <ameydabhade4@gmail.com>
2025-06-23 15:59:21 +0530

    url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: 'https://wallet-fraud-backend-vjwfoqj65-ameydabhades-projects.vercel.app'
+    NEXT_PUBLIC_API_URL: 'https://wallet-fraud-backend-ameydabhade-ameydabhades-projects.vercel.app'
   }
 };
 
