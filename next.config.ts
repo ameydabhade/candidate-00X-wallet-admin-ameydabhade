16061d5077ac0a6959f930cba7f8067e0c546ac7
Amey Dabhade <ameydabhade4@gmail.com>
2025-06-23 15:55:14 +0530

    live updat

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://wallet-fraud-backend-vjwfoqj65-ameydabhades-projects.vercel.app'
+  }
 };
 
 export default nextConfig;
